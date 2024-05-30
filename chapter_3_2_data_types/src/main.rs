61343021ced1e58f07f548b73d35651ef6b9f830
ihsan215 <aliihsantas34@gmail.com>
2024-05-30 15:14:18 +0300

    added ch3.2

---
+++ chapter_3_2_data_types/src/main.rs
@@ -0,0 +1,70 @@
+use std::io;
+
+fn main() {
+
+    // Data Types
+
+    // 1: Scaler types : A scalar type represents a single value.
+    // Rust has four primary scalar types: integers, floating-point numbers, Booleans, and characters.
+
+    // for integer usize or isize -> depend on the architecture of the computer your program is running on, which is denoted in the table as “arch”: 64 bits if you’re on a 64-bit architecture and 32 bits if you’re on a 32-bit architecture.
+
+
+    let x = 2.0; // f64
+    let y: f32 = 3.0; // f32
+
+    // addition
+    let sum = 5 + 10;
+
+     // subtraction
+    let difference = 95.5 - 4.3;
+   
+     // multiplication
+    let product = 4 * 30;
+   
+     // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+   
+     // remainder
+    let remainder = 43 % 5;
+
+    let t = true;
+
+    let f: bool = false; // with explicit type annotation
+   
+    let c = 'z';
+    let z: char = 'ℤ'; // with explicit type annotation
+    let heart_eyed_cat = '😻';
+
+
+    // Compound Types
+
+    // A tuple Types
+
+    let tup: (i32,f64,u8) = (500,6.3,1);
+    let (x,y,z) = tup;
+    println!("The value of y is: {y}");
+    let five_hundred = tup.0;
+    println!("The value of five_hundred is: {five_hundred}");
+
+
+    // Array types
+    let a = [1,2,3,4,5];
+    let b: [i32;5] = [1,2,3,4,5];
+
+    let c = [3;5]; // [3,3,3,3,3]
+
+    println!("Please enter an array index");
+
+    let mut index = String::new();
+    io::stdin().read_line(&mut index).expect("Failed to red line");
+
+    let index: usize = index.trim().parse().expect("Index enterred was not a number");
+
+    let element = a[index];
+    println!("The value of the element at index {index} is: {element}");
+
+
+ 
+}
