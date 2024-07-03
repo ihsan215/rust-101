9495def23833c01e6d0650d75004bc8e2a4ccc03
ihsan215 <aliihsantas34@gmail.com>
2024-07-03 17:55:56 +0300

    edited ch14

---
+++ chapter_14_smart_pointers/ex_1_box/src/main.rs
@@ -0,0 +1,29 @@
+// enum List {
+//     Cons(i32, Box<List>),
+//     Nil,
+// }
+
+// use crate::List::{Cons, Nil};
+
+fn main() {
+    // let b = Box::new(5);
+    // println!("b = {b}");
+
+    // let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+
+
+}
